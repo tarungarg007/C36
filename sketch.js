60f022d06c41509916fd2f938a1a5efae7de7336
tarungarg007 <73391849+tarungarg007@users.noreply.github.com>
2021-01-19 19:29:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var database;
+var position;
+var gameState = 0;
+var playerCount=0;
+
+var database;
+
+var form, player, game;
+
+function setup(){
+  database = firebase.database();
+  console.log(database);
+  createCanvas(500,500);
+  game=new Game();
+  game.getState();
+  game.start();
+
+}
+
+function draw(){
+}
+
